578213291a3f94fe3e319a16b108ef642b6a06bb
Kaviya04M <mkaviyapriya04@gmail.com>
2023-08-05 00:56:00 +0530

    Linking HTML

---
+++ js/LinkForHtml.js
@@ -0,0 +1,22 @@
+// console.log("Hello 3rd years")
+
+// document.write("we are 3rd years")
+// alert("hello guys")
+// windows.print()
+
+// const val = document.getElementById("hello") = "Hello guys"
+
+function myfunfor1st(){
+    document.getElementById("hello").innerHTML = "I am in my 1st year i am studying AI&DS dept"
+}
+function myfunfor2nd(){
+    document.getElementById("hello").innerHTML = "I am in my 2nd year i am studying AI&DS dept"
+
+}
+function myfunfor3rd(){
+    document.getElementById("hello").innerHTML = "I am in my 3rd year i am studying AI&DS dept"
+}
+function myfunfor4th(){
+    document.getElementById("hello").innerHTML = "I am in my 4th year i am studying AI&DS dept"
+
+}
